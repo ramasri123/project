15bb5cb5cb7190e2d74ae8fd161939905f371e84
ramasri123 <jeyakumarramasrijeyakumar@gmail.com>
2024-08-01 15:57:28 +0000

    123

--- task/src/pages/Home/index.tsx
+++ task/src/pages/Home/index.tsx
@@ -2,19 +2,21 @@ import react from "react"
 
 const task = () => {
     return(
-        <div className="h-screen bg-white">
-            <div className="navbar  w- bg-black text-white">
-                <a className="text-xl">Urban Company</a>
+        <div className="h-screen bg-zinc-200 ">
+            <div className="navbar min-h-20 bg-white text-black">
+                <div className="logo-container">
+                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_144,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1648471968852-1f2b01.png" alt="Logo" className="logo-img" />
+                </div>
                 <div className="flex justify-center">
-                <input type="text" placeholder="Search" className=" input input-bordered ml-40 bg-gray-200 w-24 md:w-auto" />
+                <input type="text" placeholder="Search" className=" input input-bordered ml-40 bg-gray-200 text-black w-24 md:w-auto" />
                 </div>
                 <div className="text-xl flex-5  ml-80 mr-none">
-                    <a className="btn btn-ghost mr-5">Beauty</a>
-                    <a className="btn btn-ghost" mr-5>Home</a>
-                    <a className="btn btn-ghost" mr-5>Native</a>
+                    <a className="btn btn-ghost text-xl mr-5">Beauty</a>
+                    <a className="btn btn-ghost text-xl mr-5">Home</a>
+                    <a className="btn btn-ghost text-xl mr-5">Native</a>
                 </div>
             </div>
-            <p className="mt-5 text-black text-2xl">New and noteworthy</p><br/>
+            <p className="mt-5 text-black text-2xl mr-5 bg-zinc-200">New and noteworthy</p><br/>
             <div className="w-80 h-80 flex gap-4">
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1687428458328-9e147d.jpeg" />
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681371120474-301f47.jpeg"/>
