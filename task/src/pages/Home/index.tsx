01b0ba08e7d5431c370edc9412c7716c7b1ade81
ramasri123 <jeyakumarramasrijeyakumar@gmail.com>
2024-08-03 18:41:05 +0000

    123

--- task/src/pages/Home/index.tsx
+++ task/src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import react from "react"
 import { FiShoppingCart } from "react-icons/fi";
 import { VscAccount } from "react-icons/vsc";
-import { BsSearch } from "react-icons/bs";
+import { CiSearch } from "react-icons/ci";
+import { SlLocationPin } from "react-icons/sl";
+import { IoIosArrowDown } from "react-icons/io";
 
 const task = () => {
     return(
@@ -15,36 +17,44 @@ const task = () => {
                     <a className="btn btn-ghost text-sm mr-5  bg-white hover:bg-sky-100">Home</a>
                     <a className="btn btn-ghost text-sm mr-5  bg-white hover:bg-sky-100">Native</a>
                 </div>
-                <div className="flex justify-center ml-70">
-                <input type="text"  placeholder="Search" className=" input input-bordered ml-40 border-amber-50 bg-white text-black w-24 md:w-auto" />
+                <div className=" input input-bordered ml-20 border-slate-300 bg-white text-black w-24 md:w-auto"> 
+                <SlLocationPin />
+                <input type="text" placeholder="Tirunelveli" className="ml-2"></input>  
+                <IoIosArrowDown />
                 </div>
-                <div className=" ml-60 mr-1">
+                <div className="input input-bordered ml-40 border-slate-200 bg-white text-black w-24 md:w-auto">
+                <CiSearch />
+                <input type="text"  placeholder="Search" className="ml-2 " />
+                </div>
+                <div className=" ml-20 mr-5">
                 <FiShoppingCart /></div>
-                <div className="mr-none ml-6">
+                <div tabIndex={0} className="mr-none ml-2">
                 <VscAccount />
                 </div>
+                
             </div>
-            <div className="flex w-30 h-50">
-            <div className="w-80 ml-5 mt-5">
+            <div className="flex ">
+            <div className="w-80 ml-5 mr-2 mt-5 ring-offset-0 ring ring-slate-800">
                 <div className="mx-auto text-center p-20 bg-stone-200 bg-border-black p-10 rounded-lg shadow-md overflow-hidden">
                 <h1 className="font-semibold text-black">What are You Looking For?</h1><br/>
-                <div className="flex flex row ml-2 mr-2">
+                <div className="flex flex row ">
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1678864013225-bfc1de.jpeg"/>
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1710241114433-5cfa7c.jpeg"/>
                 <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1679292077307-6143d7.jpeg"/>
                 </div>
-                <div className="flex flex row ml-2 mr-2">
+                <div className="flex flex row ">
                     <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681711961404-75dfec.jpeg"/>
                     <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1658402794135-faf080.png"/>
                     <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1705340729734-0a23f7.jpeg"/>
                 </div>
-                <div className="flex flex row ml-2 mr-2">
+                <div className="flex flex row  mr-2">
                     <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1705340800795-115b3b.jpeg"/>
                     <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_56,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1674120935535-f8d5c8.jpeg"/>
                 </div>
                 </div>
                 </div>
-                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1696852847761-574450.jpeg"/>
+                <div className="min-h-20 w-50 ml-60 mr-6 mt-5">
+                <img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto/dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1696852847761-574450.jpeg"/></div>
                 </div>
             <p className="mt-5 text-black text-2xl text-bold mr-5">New and noteworthy</p><br/>
             <div className="flex flex-row">
@@ -61,6 +71,22 @@ const task = () => {
                 <div className="w-80 h-80 ml-2 "><img src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template/w_233,dpr_1,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/luminosity/1651040420198-fe6d1d.jpeg" />
                 <a className="text-xl text-black">Hair Studio for Women</a></div>
            </div>
+           <div className="flex mt-6">
+            <div className="space-between ml-8 ">
+            <h1 className="font-semibold text-black text-2xl ">Company</h1><br/>
+                <a className="text-slate-500">About us <br/>Terms & conditions <br/>Privacy policy <br/>Anti-discrimination policy <br/>UC impact <br/>Careers </a>
+            </div>
+            <div className="space-between ml-20 ">
+            <h1 className="font-semibold text-black text-2xl ">For customers</h1><br/>
+                <a className="text-slate-500">UC reviews<br/> Categories near you<br/> Blog <br/>Contact us </a>
+            </div>
+            <div className="space-between ml-20 ">
+            <h1 className="font-semibold text-black text-2xl ">For partners</h1><br/>
+                <a className="text-slate-500">Register as a professional  </a>
+            </div>
+            <div className="space-between ml-20 ">
+            <h1 className="font-semibold text-black text-2xl ">Social links</h1><br/></div>
+            </div>
             </div>
     )
 };
